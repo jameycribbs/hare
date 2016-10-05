3021dfc2b2188f13f58abe5221d455ca1765635e
Jamey Cribbs <jamey.cribbs@gmail.com>
2016-10-05 16:47:38 -0400

    add code to create and remove test data dir

--- db_test.go
+++ db_test.go
@@ -2,10 +2,11 @@ package hare
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"testing"
 )
+
+const dataDir = "test_data"
 
 var db *database
 
@@ -23,21 +24,34 @@ func (foo *Foo) GetID() int {
 }
 
 func setup() {
-	var err error
-
-	files, _ := ioutil.ReadDir("test_data")
-	for _, file := range files {
-		if !file.IsDir() {
-			err = os.Remove("test_data/" + file.Name())
-			if err != nil {
-				fmt.Println("Failed to remove file", err)
-			}
+	_, err := os.Stat(dataDir)
+	if err == nil {
+		err = os.RemoveAll(dataDir)
+		if err != nil {
+			fmt.Println("Failed to remove test directory:", err)
+			os.Exit(1)
 		}
+	} else if !os.IsNotExist(err) {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err = os.MkdirAll("test_data", 0777); err != nil {
+		fmt.Println("Failed to make test directory:", err)
+		os.Exit(1)
 	}
 
 	db, err = OpenDB("test_data")
 	if err != nil {
 		fmt.Println("Failed to open database:", err)
+		os.Exit(1)
+	}
+}
+
+func teardown() {
+	err := os.RemoveAll(dataDir)
+	if err != nil {
+		fmt.Println("Failed to remove test directory:", err)
 		os.Exit(1)
 	}
 }
@@ -68,6 +82,8 @@ func TestCreate(t *testing.T) {
 	if foo.Bar != "test" {
 		t.Error("Expected 'test', got ", foo.Bar)
 	}
+
+	teardown()
 }
 
 func TestUpdate(t *testing.T) {
@@ -105,6 +121,8 @@ func TestUpdate(t *testing.T) {
 	if foo.Bar != "test2" {
 		t.Error("Expected 'test2', got ", foo.Bar)
 	}
+
+	teardown()
 }
 
 func TestDestroy(t *testing.T) {
@@ -136,4 +154,5 @@ func TestDestroy(t *testing.T) {
 		t.Error("Expected Find error, got no error.")
 	}
 
+	teardown()
 }
